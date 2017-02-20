b4dcee83a8c3a754486fb86e0956472c32adea40
georginajuarez <mgeorgina19@gmail.com>
2017-02-19 19:21:03 -0800

    changes per assignment

--- js/main.js
+++ js/main.js
@@ -6,3 +6,92 @@
 // 3. Connect the validation object to an event handler tied to the submit button.
 
 // Refer to the `index.html` file for the validation rules that must be enforced.
+
+//Atom didn't save my work any of the times that I was hitting save the first time, agh!
+
+$(document).on('ready', function(){
+
+//custom addMethod for comments
+$.validator.addMethod('commentLength', function( value, element ) {
+  return value.length <=500;
+}, 'Must be less than 500 characters.');
+
+
+    $('#order-form').validate({
+        submitHandler: function(form) {
+            form.submit();
+        },
+
+        rules: {
+            "your-name": {
+                required: true,
+                maxlength: 128,
+                lettersonly: true
+            },
+
+            "your-address": {
+              required: true
+            },
+
+            "your-city": {
+              required: true
+            },
+
+            "your-state": {
+                required: true,
+                maxlength: 2,
+                stateUS: true,
+                caseSensitive: false,
+                includeTerritories: false,
+                includeMilitary: false
+            },
+
+            "your-zip": {
+                required: true,
+                minlength: 5,
+                digits: true,
+                integer: true
+            },
+
+            "card-holder-name": {
+                required: true,
+                maxlength: 128,
+                lettersonly: true,
+                equalTo: "#your-name"
+                //add message that card holder name needs to match your-name
+            },
+
+            "card-number": {
+                required: true,
+                creditcard: true
+            },
+
+            "expiry-month": {
+              required: true
+            },
+
+            "expiry-year": {
+              required: true
+            },
+
+            "cvv": {
+                required: true,
+                minlength: 3,
+                digits: true
+            },
+
+            "shipping-method": {
+              required: true
+            },
+
+            "comments": {
+            commentLength: true
+          } //Could have used maxlength but wanted to try creating my own .addMethod
+
+        } //rules
+    }); //.validate
+
+
+    // Tooltips
+    $('label span.glyphicon').tooltip();
+}); //on ready function
